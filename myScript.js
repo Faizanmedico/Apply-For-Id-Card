a2ca486ae7db40d5547cb500f2d9f3a26ce89b46
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-02-14 16:02:56 +0500

    Add files via upload
    Apply For Id Card Help of JavaScript.....

---
+++ myScript.js
@@ -0,0 +1,13 @@
+let conf = confirm("Are you appling for ID card.");
+let id = confirm("Have you Old ID card or B-form Number.");
+let name = prompt("What's your name?");
+let age = prompt("What's your age?");
+let num = prompt("Id card or B-form No?")
+if (age>=18) {
+alert("Now,your card is in processing!");
+}
+else {
+alert("Sorry! You are incompetent.");
+
+}
+
